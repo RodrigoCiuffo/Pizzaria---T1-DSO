1d9e80f66b858ca77fbdceb9ddda9e6759b60a9a
Pedro Alencar <pedro.canada10@outlook.com>
2024-05-19 20:06:14 -0300

    enum classes made

---
+++ Entidades/Enum/nome_ingrediente.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class NomeIngrediente(Enum):
+    calabresa = 'calabresa'
+    molho = 'molho de tomate'
+    queijo = 'queijo mussarela'
+    tomate = 'tomate'
+    cebola = 'cebola'
+    azeitona = 'azeitona'
+    ovo = 'ovo cozido'
+    presunto = 'presunto'
+    frango = 'peito de frango'
+    catupiry = 'catupiry'
